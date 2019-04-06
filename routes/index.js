cdf2dc5103cacc39ccf33f36f0243e29920d585c
Robert <roadstar93@gmail.com>
2019-04-06 17:19:11 +0300

    Create partials and add index routes

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,25 @@ var express = require("express"),
     router = express.Router();
 
 
-//main routes
+//main route
 router.get("/", function(req, res){
     res.render("index")
+});
+
+//register route
+router.get("/register", function(req, res){
+    res.send("Working on it");
+});
+
+//login route
+router.get("/login", function(req, res){
+    res.send("Working on it");
+});
+
+
+//logout route
+router.get("/logout", function(req, res){
+    res.send("Working on it");
 });
 
 module.exports = router;
